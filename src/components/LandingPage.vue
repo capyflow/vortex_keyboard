3aaa06f4a92db681de3039ed795efdfd07691ae5
aaron <aaron@example.com>
2026-03-12 22:47:36 +0800

    chore: 更新首页版本号为 v1.0.0

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -106,7 +106,7 @@
           <span class="link-icon">↗</span>
         </a>
         <span class="footer-divider">•</span>
-        <span class="footer-version">v0.7.0</span>
+        <span class="footer-version">v1.0.0</span>
       </div>
     </div>
 
